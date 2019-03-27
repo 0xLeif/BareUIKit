6596724e239c3444378310828e9a615777628455
Zach 'Leif' Eriksen <zmeriksen@gmail.com>
2019-03-27 17:32:26 -0500

    Initial Commit

---
+++ Bare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bare
+//
+//  Created by Zach Eriksen on 3/27/19.
+//  Copyright © 2019 ol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
