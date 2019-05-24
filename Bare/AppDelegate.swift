cf7eb26c9c3ae6eaef358741ca96540484ef1dd8
Zach 'Leif' Eriksen <zmeriksen@gmail.com>
2019-05-23 21:17:39 -0500

    init push

--- Bare/AppDelegate.swift
+++ Bare/AppDelegate.swift
@@ -8,14 +8,137 @@
 
 import UIKit
 
+protocol BuilderView: UIView {
+    associatedtype ViewType
+    init()
+    func with<T>(_ property: ReferenceWritableKeyPath<ViewType, T>, value: T) -> ViewType
+    func add(toView view: UIView?) -> ViewType
+}
+
+protocol ContainerView: BuilderView {
+    init(customView: UIView)
+    
+    func add(child view: UIView?) -> Self
+    func add(children views: UIView...) -> Self
+}
+
+class BareView: UIView, BuilderView {
+    typealias ViewType = BareView
+    
+    @discardableResult
+    func add(toView view: UIView?) -> Self {
+        guard let view = view else { fatalError("View is nil") }
+        view.addSubview(self)
+        return self
+    }
+    
+    @discardableResult
+    func with<T>(_ property: ReferenceWritableKeyPath<ViewType, T>, value: T) -> Self {
+        self[keyPath: property] = value
+        return self
+    }
+}
+
+class View: BareView, ContainerView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(customView: UIView) {
+        super.init(frame: .zero)
+        add(child: customView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @discardableResult
+    func add(child view: UIView?) -> Self {
+        guard let view = view else { fatalError("View is nil") }
+        addSubview(view)
+        return self
+    }
+
+    @discardableResult
+    func add(children views: UIView...) -> Self {
+        views.forEach { view in addSubview(view) }
+        return self
+    }
+}
+
+class BaseBView: BareView {
+    var navigationView: NavigationBView = .view
+    var contentView: ContentBView = .view
+    
+    init() {
+        super.init(frame: UIScreen.main.bounds)
+        navigationView.add(toView: self)
+        contentView.add(toView: self)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class NavigationBView: View {
+    static var view: NavigationBView = {
+        return NavigationBView()
+            .with(\.frame, value: CGRect(origin: .zero,
+                                         size: CGSize(width: UIScreen.main.bounds.width,
+                                                      height: 60)))
+            .with(\.backgroundColor, value: .darkGray)
+    }()
+}
+
+class ContentBView: View {
+    static var view: ContentBView = {
+        return ContentBView()
+            .with(\.frame, value: CGRect(origin: CGPoint(x: 0, y: 60),
+                                         size: CGSize(width: UIScreen.main.bounds.width,
+                                                      height: UIScreen.main.bounds.height - 60)))
+            .with(\.backgroundColor, value: .brown)
+    }()
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Create the root window
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        // Create the base view
+        let base = BaseBView()
+            .add(toView: window)
+
+        // Add subview
+        View()
+            .with(\.frame, value: window?.bounds ?? .zero)
+            .with(\.backgroundColor, value: .red)
+            .with(\.layer.cornerRadius, value: 16)
+            .add(child: BareView()
+                .with(\.frame, value: CGRect(x: 50, y: 50, width: 100, height: 100))
+                .with(\.backgroundColor, value: .black))
+            .add(children:
+                BareView()
+                    .with(\.frame, value: CGRect(x: 50, y: 100, width: 50, height: 30))
+                    .with(\.backgroundColor, value: .black),
+                 BareView()
+                    .with(\.frame, value: CGRect(x: 350, y: 50, width: 100, height: 300))
+                    .with(\.backgroundColor, value: .black),
+                 BareView()
+                    .with(\.frame, value: CGRect(x: 450, y: 450, width: 100, height: 300))
+                    .with(\.backgroundColor, value: .black))
+            .add(toView: base)
+
+
         return true
     }
 
@@ -40,7 +163,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
